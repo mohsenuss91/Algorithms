a762548c89f2051333fa45f6eb26cb1bd3333671
redouane <unrealdz@gmail.com>
2011-03-06 01:19:45 +0100

    multiply without the * operator, using C

---
+++ Math/Multiply without operator/multiply.c
@@ -0,0 +1,27 @@
+// Redouane <unrealdz@gmail.com>
+// Multiply two ints without using the * operator 
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char *argv[]) {
+	int x,y;
+	printf("Type a number : ");
+	scanf("%d", &x);
+	printf("Type a second number : ");
+	scanf("%d", &y);
+	printf("%d * %d = %d", x,y, multiply(x,y));
+	
+}
+
+
+int multiply(int a, int b) {
+	int i;
+	int res = 0;
+
+	for (i=1;i<=b;i++) {
+		res = res + a;
+	}
+
+	return res;
+}
